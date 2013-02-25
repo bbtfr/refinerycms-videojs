ca7a7f0027d6e8141ce7e32c11a7569d3aef5bbd
LiYang <theo@secondbureau.com>
2013-02-25 13:24:58 +0800

    add video gallery

--- lib/generators/refinery/videos_generator.rb
+++ lib/generators/refinery/videos_generator.rb
@@ -14,5 +14,14 @@ module Refinery
       template "assets/javascripts/videojs_loader.js", File.join(destination_root, "app", "assets", "javascripts", "videojs_loader.js")
     end
 
+    def append_load_seed_data
+      create_file 'db/seeds.rb' unless File.exists?(File.join(destination_root, 'db', 'seeds.rb'))
+      append_file 'db/seeds.rb', :verbose => true do
+        <<-EOH
+
+# Added by Refinery CMS VideoJS extension
+Refinery::Videos::Engine.load_seed
+        EOH
+      end
   end
 end
